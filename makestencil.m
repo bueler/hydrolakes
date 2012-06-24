5ba93d97a985df39ff0b7db8470fe07bebcf15a8
Ed Bueler <bueler@pismdocs.org>
2012-06-23 16:22:00 -0800

    use matlab to make better stencil

--- makestencil.m
+++ makestencil.m
@@ -2,7 +2,7 @@ function makestencil
 % MAKESTENCIL   Plot a stencil.  Works under Matlab better than Octave, especially
 % because saving the figures sucks under Octave.
 
-msize=20;
+msize=16;
 
 basic(1) % stencil for non-constant diffusivity
 hold on
@@ -11,11 +11,11 @@ plot([-1 0 1 0 0], [0 0 0 -1 1], 'dk', 'Markersize',msize)
 hold off
 % to create .eps in Octave:
 %print -depsc2 ../pdffigs/diffstencil.eps
-print -dpdf diffstencil.pdf
+%print -dpdf diffstencil.pdf
 
    function basic(i)
    figure(i)
-   lwidth = 8.0;
+   lwidth =4.0;
 
    % make the grid
    x=-1.3:0.1:1.3;
@@ -29,14 +29,14 @@ print -dpdf diffstencil.pdf
 
    % make the cell
    xcell=-0.5:0.1:0.5;
-   clwidth=4.0;
+   clwidth=2.0;
    plot(xcell,[-0.5*ones(size(xcell)); 0.5*ones(size(xcell))]','k--',...
         'LineWidth',clwidth)
    plot([-0.5*ones(size(xcell)); 0.5*ones(size(xcell))],xcell','k--',...
         'LineWidth',clwidth)
 
    % label the regular grid positions
-   fsize = 20;
+   fsize = 16;
    text(-1,-1.5,'i-1','FontSize',fsize,'FontAngle','italic',...
       'HorizontalAlignment','center')
    text(0,-1.5,'i','FontSize',fsize,'FontAngle','italic',...
