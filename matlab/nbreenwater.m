6792813b7ec7ef8f02394a698901ef04615e395a
Ed Bueler <elbueler@alaska.edu>
2012-11-01 12:10:29 -0800

    cleaning up as I get back to thinking about this topic ...

--- matlab/nbreenwater.m
+++ matlab/nbreenwater.m
@@ -1,8 +1,8 @@
 function [W, P] = nbreenwater(tyears,nn,W0,P0)
 % NBREENWATER  **FIXME this comment**
-% Form:  W = nbreenwater(tyears,nn)
-% (Note all input and output arguments are optional.)
-% Calls:   BUILDNBREEN, CONSERVEWATER, DAMPER
+% Form:  [W, P] = nbreenwater(tyears,nn,W0,P0)
+% All input and output arguments are optional (and have given defaults).
+% Calls:   BUILDNBREEN, CONSERVEWATER, DOUBLEDIFF
 % Depends: NETCDF
 
 if nargin<1, tyears=5.0; end
@@ -34,12 +34,10 @@ for i=1:length(x)
     for j=1:length(y)
         Phi0arr(i,j) = (3.0/900.0)*(900.0-min(usurf(i,j),900.0))/spera; 
         if outline(i,j)>0.5, Phi(i,j) = Phi0arr(i,j); end
-        %if usurf(i,j)==0.0, usurf(i,j)=topg(i,j); end
     end
 end
 
 if true
-%if nargout < 1
   fprintf('showing initial fields: topg, usurf, outline, Phi, geometric psi\n')
 
   figure(1)
@@ -80,7 +78,6 @@ if nargin < 3
   W0 = zeros(size(topg));
 end
 if nargin < 4
-  %Y0 = W0;
   %P0 = rhoi * g * thk;
   P0 = zeros(size(topg));
 end
@@ -89,16 +86,12 @@ vb = 50.0/spera;    % ice speed (m s-1; = 50 m a-1)
 magvb = vb * ones(size(topg));
 ts = 0.0;
 te = tyears*spera;
+fprintf('calling doublediff() to do run for %.3f years:\n\n',tyears)
 %W = conservewater(x,y,topg,usurf,outline,W0,Phi,ts,te,5);
-if true
-  [W, P] = doublediff(x,y,topg,usurf,magvb,outline,W0,P0,Phi,ts,te,5);
-else
-  [W, Y, P] = damper(x,y,topg,usurf,magvb,outline,W0,Y0,Phi,ts,te,5);
-end
+%[W, Y, P] = damper(x,y,topg,usurf,magvb,outline,W0,Y0,Phi,ts,te,5);
+[W, P] = doublediff(x,y,topg,usurf,magvb,outline,W0,P0,Phi,ts,te,5);
 
 if true
-%if nargout < 1
-  %fprintf('showing final fields: W, Y, P\n')
   fprintf('showing final fields: W, P\n')
 
   figure(6)
@@ -106,17 +99,12 @@ if true
   title('water thickness W  (m)')
   xlabel('x (km)'), ylabel('y (km)')
 
-  %figure(7)
-  %imagesc(x/1000,flipud(-y)/1000,flipud(Y)), colorbar
-  %title('capacity thickness Y  (m)')
-  %xlabel('x (km)'), ylabel('y (km)')
-
-  figure(8)
+  figure(7)
   imagesc(x/1000,flipud(-y)/1000,flipud(P)), colorbar
   title('water pressure P  (Pa)')
   xlabel('x (km)'), ylabel('y (km)')
 
-  figure(9)
+  figure(8)
   Pmask = ones(size(P));
   Po = rhoi * g * thk;
   Pmask(P < 0.001*Po) = 0;
