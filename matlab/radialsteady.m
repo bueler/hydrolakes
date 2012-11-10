8a7cb3e5ba0e85b31a6e094046ba6e151411917c
Ed Bueler <elbueler@alaska.edu>
2012-11-10 14:32:06 -0900

    octave is irritating ... have to switch to matlab to get full effect here

--- matlab/radialsteady.m
+++ matlab/radialsteady.m
@@ -15,24 +15,42 @@ Phi0 = 0.2 / p.spera;    % m/s   water input rate is 20 cm/a
 vphi0 = Phi0 / (2 * p.c0);
 
 if dofigs
-  % ice thickness and sliding velocity for plot
-  dr = R0 / 500;
-  rr = 0:dr:R0;
-  hrr = h0 * (1 - (rr/R0).^2);
-  vbrr = v0 * (rr - R1).^5 / (R0-R1)^5;
-  vbrr(rr < R1) = 0.0;
-
   set(0,'defaultaxesfontsize',14)
   set(0,'defaultlinelinewidth',3.0)
-  figure(97), clf
-  ax = plotyy(rr/1000.0,hrr,rr/1000.0,vbrr * p.spera);
+
+  % ice thickness and sliding velocity for plot
+  dr = R0 / 500;  r = 0:dr:R0;
+  h = h0 * (1 - (r/R0).^2);
+  vb = v0 * (r - R1).^5 / (R0-R1)^5;
+  vb(r < R1) = 0.0;
+  figure(96), clf
+  ax = plotyy(r/1000.0,h,r/1000.0,vb * p.spera);
   xlabel('r  (km)')
   ylabel(ax(1),'h = ice thickness  (m)')
   ylabel(ax(2),'|v_b| = ice sliding speed   (m/a)')
 
-  clear dr rr hrr vbrr
+  r = 0:dr:R0-dr;  % avoid location where Po=0
+  dW = p.Wr/500;
+  W = dW:dW:1.2*p.Wr;
+  [rr WW] = meshgrid(r,W);
+  Po = p.rhow * p.g * h0 * (1 - (rr/R0).^2);
+  vb = v0 * (rr - R1).^5 / (R0-R1)^5;
+  vb(rr < R1) = 0.0;
+  ratio = Psteady(p,Po,vb,WW) ./ Po;
+  figure(97), clf
+  [cc ll] = contour(rr/1000.0,WW,ratio,[0.000001 0.999999],'linewidth',3.0);
+  %clabel(cc,ll,'fontsize',14.0);
+  text(2,0.5,'O','fontsize',16.0)
+  text(12,1.1,'O','fontsize',16.0)
+  text(12,0.5,'N','fontsize',16.0)
+  text(22,0.3,'U','fontsize',16.0)
+  xlabel('r  (km)'), ylabel('W  (m)')
+  axis([0 R0/1000.0 0 1.2*p.Wr]), grid on
+
+  clear dr r rr h Po vb W WW
 end
 
+return
 % in octave this requires "odepkg", but then fails because of negative step direction
 
 % solve the ODE
@@ -57,7 +75,7 @@ vb(r > R0) = 0.0;
 h = h0 * (1 - (r/R0).^2);
 h(r > R0) = 0.0;
 Po = p.rhoi * p.g * h;
-P = Psteady(Po,vb,W);
+P = Psteady(p,Po,vb,W);
 
 if dofigs
   % show pressure solution
@@ -88,11 +106,15 @@ end
   dW    = numer ./ denom;
   end
 
-  function P = Psteady(Po,vb,W)
+  function P = Psteady(p,Po,vb,W)
   % this function computes P(W) in steady state
-  if any(Po < 0), error('Psteady() requires nonnegative overburden pressure Po'), end
-  if any(vb < 0), error('Psteady() requires nonnegative sliding speed vb'), end
-  if any(W <= 0), error('Psteady() requires positive water thickness W'), end
+  % it is vectorized in all arguments
+  if any(any(Po < 0))
+    error('Psteady() requires nonnegative overburden pressure Po'), end
+  if any(any(vb < 0))
+    error('Psteady() requires nonnegative sliding speed vb'), end
+  if any(any(W <= 0))
+    error('Psteady() requires positive water thickness W'), end
   ff = (p.Wr - W) ./ W;
   ff(ff < 0) = 0.0;
   CC = p.c1 / (p.c2 * p.A);
