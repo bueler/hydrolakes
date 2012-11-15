bb55191fe83ddb50e0d55ffb97027f781ccc0907
Ed Bueler <elbueler@alaska.edu>
2012-11-15 14:39:09 -0900

    code that generated new figures in previous commit

--- matlab/plotpsteady.m
+++ matlab/plotpsteady.m
@@ -12,7 +12,7 @@ v0   = 100.0 / p.spera;  % m/s; base sliding rate
 
 % full range of W for vb=0 and F&C cases
 W = 0.0:0.0005:1.2*p.Wr;
-Pno  = PofW(p,W,    0.0,Po) / 1e5;
+Pno  = PofW(p,W,0.0,Po) / 1e5;
 % compare Flowers and Clarke function with W_crit = W_r
 PFC  = Po * (W / p.Wr).^(7/2) / 1e5;
 
@@ -22,7 +22,7 @@ set(0,'defaultaxesfontsize',16.0)
 figure(1)
 plot(W,Pno,'b',W,PFC,'k--')
 hold on
-sty = ['g'; 'r'; 'c'];
+sty = ['b'; 'g'; 'r'; 'c'];
 aa = [0.1 1.0 10.0];
 for j=1:3
   vb = aa(j) * v0;
@@ -30,39 +30,40 @@ for j=1:3
   Wc = sbcube * p.Wr - Po^3 * p.Y0;
   Wc = Wc / (sbcube + Po^3);
   Pc = PofW(p,Wc,vb,Po);
-  plot(Wc,Pc,'ko','markersize',9)
   fprintf('for vb = %8.3f m/a, Wc = %9.6f m and Pc = %10.3f Pa\n',...
           vb * p.spera, Wc, Pc)
   W = Wc:0.0005:1.2*p.Wr;
   P = PofW(p,W,vb,Po) / 1e5;
-  plot(W,P,sty(j,:))
+  plot(W,P,sty(j+1,:))
+  plot(Wc,Pc,'k.','markersize',30)
 end
 hold off
-%legend('no sliding; P=P_o  ','|v_b|=10 m/a','|v_b|=100 m/a','|v_b|=1000 m/a',...
-%       'location','southeast')
 xlabel('W  (m)'), ylabel('P  (bar)')
 axis([0 max(W) 0 1.3*Po/1e5])
-text(0.05,108,'(a)','fontsize',18)
 
 figure(2)
-W = 0.0:0.0005:1.2*p.Wr;
-PP1 = PofW(p,W,v0,0.2*Po) / 1e5; % H =  200 m
-PP2 = PofW(p,W,v0,0.5*Po) / 1e5; % H =  500 m
-PP3 = PofW(p,W,v0,Po) / 1e5;     % H = 1000 m
-PP4 = PofW(p,W,v0,2*Po) / 1e5;   % H = 2000 m
-plot(W,PP1,W,PP2,W,PP3,W,PP4);
-legend('H = 200 m','H = 500 m','H = 1000 m','H = 2000 m',...
-       'location','east')
+bb = [2.0 1.0 0.5 0.2];  % H = 2000, 1000, 500, 200 m
+for j=1:4
+  myPo = bb(j) * Po;
+  sbcube = p.c1 * v0 / (p.c2 * p.A);
+  Wc = sbcube * p.Wr - myPo^3 * p.Y0;
+  Wc = Wc / (sbcube + myPo^3);
+  Pc = PofW(p,Wc,v0,myPo);
+  W = Wc:0.0005:1.2*p.Wr;
+  P = PofW(p,W,v0,myPo) / 1e5;
+  plot(W,P,sty(j,:))
+  hold on
+  plot(Wc,Pc,'k.','markersize',30)
+end
+hold off
 xlabel('W  (m)'), ylabel('P  (bar)')
 axis([0 max(W) 0 1.15*2*Po/1e5])
-text(0.05,1.08*2*Po/1e5,'(b)','fontsize',18)
 
   function P = PofW(p,W,vb,Po)
     sbcube = p.c1 * vb / (p.c2 * p.A);
     frac = max(0.0, p.Wr - W) ./ (W + p.Y0);
     P = Po - (sbcube * frac).^(1/3);
-    P(P<0.0) = 0.0;
-    P(P>Po) = Po;
+    P(P < 0.0) = 0.0;
   end
 
 end
