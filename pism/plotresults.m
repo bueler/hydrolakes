943aacae1885346ce7c03dd8325419975a539c02
Ed Bueler <elbueler@alaska.edu>
2013-01-01 08:40:55 -0900

    add figures for PISM results; because Matlab code allowed comparison between limiter and first-order, let's hold on to redundant results for now

--- pism/plotresults.m
+++ pism/plotresults.m
@@ -11,7 +11,7 @@ figure(1)
 loglog(dx,averrW,'ks',dx,maxerrW,'kx','markersize',12)
 grid on,  xlabel('\Delta x  (m)'), ylabel('error in W  (m)')
 axis([0.9*min(dx) 1.1*max(dx) 0.9*min(averrW) 1.1*max(maxerrW)])
-set(gca,'XTick',fliplr(dx))
+set(gca,'XTick',flipud(dx))
 legend('average W error','maximum W error','Location','SouthEast')
 print -dpdf refineWpism.pdf
 
@@ -19,7 +19,7 @@ figure(2)
 loglog(dx,averrP,'ks',dx,maxerrP,'kx','markersize',12)
 grid on,  xlabel('\Delta x  (m)'), ylabel('error in P  (bar)')
 axis([0.9*min(dx) 1.1*max(dx) 0.9*min(averrP) 1.1*max(maxerrP)])
-set(gca,'XTick',fliplr(dx))
+set(gca,'XTick',flipud(dx))
 legend('average P error','maximum P error','Location','SouthEast')
 print -dpdf refinePpism.pdf
 
