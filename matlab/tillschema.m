73f560d243b1c15e6fc97431269a9713d9ddee4b
Ed Bueler <elbueler@alaska.edu>
2013-04-21 10:35:45 -0800

    till water dynamics mostly fleshed out on the continuum side

--- matlab/tillschema.m
+++ matlab/tillschema.m
@@ -6,16 +6,16 @@ da = 0.1;
 
 y = 0.4;
 plot([-0.2 2.0], [y y], 'k'), hold on,  arrow(2.0,y)
-text(2.05,y,'W_{tl}','fontsize',16)
-tick(0.0,y,'0'),  tick(0.7,y,'W'),  tick(1.0,y,'W_{tl}^{max}')
+text(2.05,y,'W_{til}','fontsize',16)
+tick(0.0,y,'0'),  tick(0.7,y,'W'),  tick(1.0,y,'W_{til}^{max}')
 plot([0.2 0.5], [y+da y+da], 'k'),  sharpright(0.5,y+da)
 plot([0.77 0.93], [y+da y+da], 'k'),  sharpleft(0.77,y+da)
 text(1.3,y+da,'not allowed','fontsize',16)
 
 y = 0.0;
 plot([-0.2 2.0], [y y], 'k'),  arrow(2.0,y)
-text(2.05,y,'W_{tl}','fontsize',16)
-tick(0.0,y,'0'),  tick(1.0,y,'W_{tl}^{max}'),  tick(1.4,y,'W')
+text(2.05,y,'W_{til}','fontsize',16)
+tick(0.0,y,'0'),  tick(1.0,y,'W_{til}^{max}'),  tick(1.4,y,'W')
 plot([0.25 0.75], [y+da y+da], 'k'),  sharpright(0.75,y+da)
 text(1.3,y+da,'not allowed','fontsize',16)
 
