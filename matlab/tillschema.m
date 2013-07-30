97872913dd5a463e52e3306da5af37e66a29b459
Ed Bueler <elbueler@alaska.edu>
2013-07-29 17:20:03 -0800

    Fixes to Figure 1 showing till update schema.

--- matlab/tillschema.m
+++ matlab/tillschema.m
@@ -3,29 +3,29 @@ function tillschema
 
 set(0,'defaultlinelinewidth',2.0)
 da = 0.1;
+rht = 1.6;
 
-y = 0.4;
-plot([-0.2 2.0], [y y], 'k'), hold on,  arrow(2.0,y)
-text(2.05,y,'W_{til}','fontsize',16)
-tick(0.0,y,'0'),  tick(0.7,y,'W'),  tick(1.0,y,'W_{til}^{max}')
+y = 0.55;
+plot([-0.2 rht], [y y], 'k'), hold on,  arrow(rht,y)
+tick(0.0,y,'0'),  tick(0.6,y,'\tau W'),  tick(1.0,y,'W_{til}^{max}')
 plot([0.2 0.5], [y+da y+da], 'k'),  sharpright(0.5,y+da)
+text(0.05,y+1.7*da,'W_{til} increases','fontsize',16)
 plot([0.77 0.93], [y+da y+da], 'k'),  sharpleft(0.77,y+da)
-text(1.3,y+da,'not allowed','fontsize',16)
+text(0.7,y+1.7*da,'W_{til} decreases','fontsize',16)
 
 y = 0.0;
-plot([-0.2 2.0], [y y], 'k'),  arrow(2.0,y)
-text(2.05,y,'W_{til}','fontsize',16)
-tick(0.0,y,'0'),  tick(1.0,y,'W_{til}^{max}'),  tick(1.4,y,'W')
+plot([-0.2 rht], [y y], 'k'),  arrow(rht,y)
+tick(0.0,y,'0'),  tick(1.0,y,'W_{til}^{max}'),  tick(1.4,y,'\tau W')
 plot([0.25 0.75], [y+da y+da], 'k'),  sharpright(0.75,y+da)
-text(1.3,y+da,'not allowed','fontsize',16)
+text(0.2,y+1.7*da,'W_{til} increases','fontsize',16)
 
 hold off
-axis([-0.3 2.1 -0.2 1.3]), axis off
+axis([-0.3 (rht+0.2) -0.2 1.3]), axis off
 end
 
   function tick(x,y,s)
     plot([x x], [y-0.03 y+0.03],'k')
-    text(x-0.01,y-0.06,s,'fontsize',16)
+    text(x-0.04,y-0.08,s,'fontsize',16)
   end
 
   function arrow(x,y)
