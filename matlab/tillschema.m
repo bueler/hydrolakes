a50cb35d86ebfbed79970d3247b836685a0dc35f
Ed Bueler <elbueler@alaska.edu>
2013-08-14 23:38:58 -0800

    Quite a bit of revision through end of Table 2 on page 14.

--- matlab/tillschema.m
+++ matlab/tillschema.m
@@ -5,9 +5,9 @@ set(0,'defaultlinelinewidth',2.0)
 da = 0.1;
 rht = 1.6;
 
-y = 0.55;
+y = 0.45;
 plot([-0.2 rht], [y y], 'k'), hold on,  arrow(rht,y)
-tick(0.0,y,'0'),  tick(0.6,y,'\tau W'),  tick(1.0,y,'W_{til}^{max}')
+tick(0.0,y,'0'),  tick(0.6,y,'\omega W'),  tick(1.0,y,'W_{til}^{max}')
 plot([0.2 0.5], [y+da y+da], 'k'),  sharpright(0.5,y+da)
 text(0.05,y+1.7*da,'W_{til} increases','fontsize',16)
 plot([0.77 0.93], [y+da y+da], 'k'),  sharpleft(0.77,y+da)
@@ -15,7 +15,7 @@ text(0.7,y+1.7*da,'W_{til} decreases','fontsize',16)
 
 y = 0.0;
 plot([-0.2 rht], [y y], 'k'),  arrow(rht,y)
-tick(0.0,y,'0'),  tick(1.0,y,'W_{til}^{max}'),  tick(1.4,y,'\tau W')
+tick(0.0,y,'0'),  tick(1.0,y,'W_{til}^{max}'),  tick(1.4,y,'\omega W')
 plot([0.25 0.75], [y+da y+da], 'k'),  sharpright(0.75,y+da)
 text(0.2,y+1.7*da,'W_{til} increases','fontsize',16)
 
