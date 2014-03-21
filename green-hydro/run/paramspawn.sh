666a33d0c18ba212b5140b789a12c9ffa846af0e
Andy Aschwanden <andy.aschwanden@gmail.com>
2014-03-20 16:42:07 -0800

    spawn script creates a bunch of short runs with varying ice dynamics and hydrology params

--- green-hydro/run/paramspawn.sh
+++ green-hydro/run/paramspawn.sh
@@ -2,7 +2,7 @@
 
 # Copyright (C) 2009-2014 Ed Bueler and Andy Aschwanden
 
-#  creates 18 scripts, each with NN processors, for a parameter study
+#  creates a bunch of scripts, each with NN processors, for a parameter study
 #  scripts are suitable for PBS job scheduler
 #  (see  http://www.adaptivecomputing.com/products/open-source/torque/)
 #
@@ -48,14 +48,44 @@ MPIQUEUELINE="#PBS -q standard_4"
 
 INFILE=g10km_100ka_const_hot.nc
 GRID=10
-DURA=2000
-for RATE in 1e-5 5e-5 1e-6 5e-6 ; do
-  for PROP in 10 50 100 150 200 ; do
+DURA=250
 
-      SCRIPT="do_rate_${RATE}_prop_${PROP}_sgl_true.sh"
+for PPQ in 0.1 0.25 0.8 ; do
+  for TEFO in 0.01 0.02 0.05 ; do
+      for RATE in 1e-5 5e-5 1e-6 5e-6 ; do
+	  for PROP in 1 10 100 1000 10000 ; do
+
+	      SCRIPT="do_ppq_${PPQ}_tefo_${TEFO}_rate_${RATE}_prop_${PROP}.sh"
+	      rm -f $SCRIPT
+	      EXPERIMENT=ppq_${PPQ}_tefo_${TEFO}_rate_${RATE}_prop_${PROP}
+	      OUTFILE=g${GRID}km_${PPQ}_${TEFO}_${RATE}_${PROP}.nc
+
+	      # insert preamble
+	      echo $SHEBANGLINE >> $SCRIPT
+	      echo >> $SCRIPT # add newline
+	      echo $MPIQUEUELINE >> $SCRIPT
+	      echo $MPITIMELINE >> $SCRIPT
+	      echo $MPISIZELINE >> $SCRIPT
+	      echo $MPIOUTLINE >> $SCRIPT
+	      echo >> $SCRIPT # add newline
+	      echo "cd \$PBS_O_WORKDIR" >> $SCRIPT
+	      echo >> $SCRIPT # add newline
+
+	      export PISM_EXPERIMENT=$EXPERIMENT
+	      export PISM_TITLE="Greenland Parameter Study"
+	      
+	      cmd="PISM_DO="" TSSTEP=daily EXSTEP=yearly PARAM_PPQ=$PPQ PARAM_TEFO=$TEFO PARAM_TWRATE=$RATE PARAM_TWPROP=$PROP ./run.sh $NN const $DURA $GRID hybrid routing $OUTFILE $INFILE"
+	      echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
+	      
+	      echo "($SPAWNSCRIPT)  $SCRIPT written"
+
+	  done
+      done
+
+      SCRIPT="do_ppq_${PPQ}_tefo_${TEFO}_hydro_null.sh"
       rm -f $SCRIPT
-      EXPERIMENT=rate_${RATE}_prop_${PROP}_sgl_true
-      OUTFILE=g${GRID}km_${RATE}_${PROP}.nc
+      EXPERIMENT=ppq_${PPQ}_tefo_${TEFO}_hydro_null
+      OUTFILE=g${GRID}km_${PPQ}_${TEFO}_hydro_null.nc
 
       # insert preamble
       echo $SHEBANGLINE >> $SCRIPT
@@ -67,14 +97,15 @@ for RATE in 1e-5 5e-5 1e-6 5e-6 ; do
       echo >> $SCRIPT # add newline
       echo "cd \$PBS_O_WORKDIR" >> $SCRIPT
       echo >> $SCRIPT # add newline
-
+      
       export PISM_EXPERIMENT=$EXPERIMENT
       export PISM_TITLE="Greenland Parameter Study"
-
-      cmd="PISM_DO="" TSSTEP=daily EXSTEP=yearly PARAM_TWRATE=$RATE PARAM_TWPROP=$PROP ./run-hydro.sh $NN const $DURA $GRID hybrid routing $OUTFILE $INFILE"
+      
+      cmd="PISM_DO="" TSSTEP=daily EXSTEP=yearly PARAM_PPQ=$PPQ PARAM_TEFO=$TEFO ./run.sh $NN const $DURA $GRID hybrid null $OUTFILE $INFILE"
       echo "$cmd 2>&1 | tee job.\${PBS_JOBID}" >> $SCRIPT
-
+	      
       echo "($SPAWNSCRIPT)  $SCRIPT written"
+
 
   done
 done
