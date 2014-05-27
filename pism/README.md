667cb3a523dacc70b094857abdf6f7ae4ea67bca
Ed Bueler <elbueler@alaska.edu>
2014-05-27 11:17:04 -0800

    making a better convergence plot

---
+++ pism/README.md
@@ -0,0 +1,6 @@
+hydrolakes/pism/
+--------
+
+Got to `PISMDIR/test/test_hydrology/` and run `verifTestP.sh` to generate
+a file that can be edited into `verifTestP.results` in this directory.  Then
+run `plotresults.m` to generate a convergence plot `refineWPpism.pdf`.
--- pism/plotresults.m
+++ pism/plotresults.m
@@ -7,23 +7,32 @@ maxerrW = verifTestP(:,3);
 averrP  = verifTestP(:,4) / 1.0e5;  % in bar
 maxerrP = verifTestP(:,5) / 1.0e5;  % in bar
 
-figure(1)
-loglog(dx,averrW,'ks',dx,maxerrW,'kx','markersize',12)
-grid on,  xlabel('\Delta x  (m)'), ylabel('error in W  (m)')
+format short g
+pW = polyfit(log(dx(1:end-1)),log(averrW(1:end-1)),1);
+pP = polyfit(log(dx(1:end-1)),log(averrP(1:end-1)),1);
+fprintf('fit to all average error data except finest grid\n')
+fprintf('  (av err bwat) = O(dx^{%.2f}),  (av err bwp) = O(dx^{%.2f})\n',pW(1),pP(1))
+
+figure(1,'Position',[100,100,600,600])
+subplot(4,1,1:2)
+loglog(dx,averrW,'ks',dx,maxerrW,'kx')
+grid on,  ylabel('error in W  (m)')
 axis([0.9*min(dx) 1.1*max(dx) 0.9*min(averrW) 1.1*max(maxerrW)])
 set(gca,'XTick',flipud(dx))
-legend('average W error','maximum W error','Location','SouthEast')
-print -dpdf refineWpism.pdf
+set(gca,'XTickLabel',{'','','','',''})
+set(gca,'YTick',[0.001 0.01])
+set(gca,'YTickLabel',{'0.001','0.01'})
+legend('average W error','maximum W error','Location','NorthWest')
 
-figure(2)
-loglog(dx,averrP,'ks',dx,maxerrP,'kx','markersize',12)
+subplot(4,1,3:4)
+loglog(dx,averrP,'ks',dx,maxerrP,'kx')
 grid on,  xlabel('\Delta x  (m)'), ylabel('error in P  (bar)')
 axis([0.9*min(dx) 1.1*max(dx) 0.9*min(averrP) 1.1*max(maxerrP)])
 set(gca,'XTick',flipud(dx))
-legend('average P error','maximum P error','Location','SouthEast')
-print -dpdf refinePpism.pdf
+set(gca,'XTickLabel',{'125','250','500','1000','2000'})
+set(gca,'YTick',[0.01 0.1])
+set(gca,'YTickLabel',{'0.01','0.1'})
+legend('average P error','maximum P error','Location','NorthWest')
 
-format short g
-pW = polyfit(log(dx(2:end-1)),log(averrW(2:end-1)),1)
-pP = polyfit(log(dx(2:end-1)),log(averrP(2:end-1)),1)
+print -dpdf refineWPpism.pdf
 
